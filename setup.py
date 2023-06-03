2a36ff5c234efb2060e022d7c5039791fe481896
JarbasAI <33701864+JarbasAl@users.noreply.github.com>
2023-06-03 16:42:46 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup
 
 # skill_id=package_name:SkillClass
-PLUGIN_ENTRY_POINT = 'mycroft-desktop-launcher.mycroftai=skill_application_launcher:ApplicationLauncherSkill'
+PLUGIN_ENTRY_POINT = 'skill-ovos-application-launcher.openvoiceos=skill_application_launcher:ApplicationLauncherSkill'
 # in this case the skill_id is defined to purposefully replace the mycroft version of the skill,
 # or rather to be replaced by it in case it is present. all skill directories take precedence over plugin skills
 
@@ -11,7 +11,7 @@ setup(
     name='skill-application-launcher',
     version='0.0.2',
     description='OVOS application launcher skill plugin',
-    url='https://github.com/JarbasSkills/skill-application-launcher',
+    url='https://github.com/OpenVoiceOS/skill-ovos-application-launcher',
     author='JarbasAi',
     author_email='jarbasai@mailfence.com',
     license='Apache-2.0',
